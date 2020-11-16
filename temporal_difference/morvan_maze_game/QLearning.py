dd9a5390648890b0ac2fdd2aa21383f6a7e192a2
GodWriter <18917565247@163.com>
2020-11-16 21:48:16 +0800

    commit.

--- temporal_difference/morvan_maze_game/QLearning.py
+++ temporal_difference/morvan_maze_game/QLearning.py
@@ -4,16 +4,41 @@ import pandas as pd
 from temporal_difference.morvan_maze_game.maze_env import Maze
 
 
+def update():
+    for episode in range(100):
+        observation = env.reset()
+
+        while True:
+            env.render()
+
+            action = RL.choose_action(str(observation))
+            observation_, reward, done = env.step(action)
+
+            RL.learn(str(observation), action, reward, str(observation_))
+            observation = observation_
+
+            if done:
+                break
+
+
 class QLearningTable:
     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
         self.actions = actions
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.action, dtype=np.float64)
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
     def choose_action(self, observation):
-        pass
+        self.check_state_exist(observation)
+
+        if np.random.uniform() < self.epsilon:
+            state_action = self.q_table.loc[observation, :]
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+        else:
+            action = np.random.choice(self.actions)
+
+        return action
 
     def check_state_exist(self, state):
         if state not in self.q_table.index:
@@ -31,3 +56,11 @@ class QLearningTable:
             q_target = r
 
         self.q_table.loc[s, a] += self.lr * (q_target - q_predict)
+
+
+if __name__ == "__main__":
+    env = Maze()
+    RL = QLearningTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
+    env.mainloop()

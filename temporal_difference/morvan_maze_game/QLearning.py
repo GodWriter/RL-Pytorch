f068bb81d67f4a7107583893bc7c94ae3aa14331
GodWriter <18917565247@163.com>
2020-11-16 20:00:08 +0800

    commit.

--- temporal_difference/morvan_maze_game/QLearning.py
+++ temporal_difference/morvan_maze_game/QLearning.py
@@ -1,1 +1,33 @@
 import numpy as np
+import pandas as pd
+
+from temporal_difference.morvan_maze_game.maze_env import Maze
+
+
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.action, dtype=np.float64)
+
+    def choose_action(self, observation):
+        pass
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(pd.Series([0] * len(self.actions),
+                                                         index=self.q_table.columns,
+                                                         name=state))
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
+        else:
+            q_target = r
+
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)

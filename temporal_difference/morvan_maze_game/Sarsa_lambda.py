bd9107fb4931a3792eeb813c713682dc2f947427
GodWriter <18917565247@163.com>
2020-11-18 13:12:41 +0800

    commit.

---
+++ temporal_difference/morvan_maze_game/Sarsa_lambda.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+
+from temporal_difference.morvan_maze_game.maze_env import Maze
+
+
+class RL(object):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+
+        if np.random.rand() < self.epsilon:
+            state_action = self.q_table.loc[observation, :]
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+        else:
+            action = np.random.choice(self.actions)
+
+        return action
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(pd.Series([0] * len(self.actions),
+                                                         index=self.q_table.columns,
+                                                         name=state))
+
+    def learn(self, *args):
+        pass
+
+
+class SarsaLambdaTable(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9, trace_decay=0.9):
+        super(SarsaLambdaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+
+        self.lambda_ = trace_decay
+        self.eligibility_trace = self.q_table.copy()
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            to_be_append = pd.Series([0] * len(self.actions),
+                                     index=self.q_table.columns,
+                                     name=state)
+
+            self.q_table = self.q_table.append(to_be_append)
+            self.eligibility_trace = self.eligibility_trace.append(to_be_append)
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.loc[s_, a_]
+        else:
+            q_target = r
+        delta = q_target - q_predict
+
+        # Method 1
+        # self.eligibility_trace.loc[s, a] += 1
+
+        # Method 2:
+        self.eligibility_trace.loc[s, :] *= 0
+        self.eligibility_trace.loc[s, a] = 1
+
+        # Q and decay eligibility update
+        self.q_table += self.lr * delta * self.eligibility_trace
+        self.eligibility_trace *= self.gamma * self.lambda_

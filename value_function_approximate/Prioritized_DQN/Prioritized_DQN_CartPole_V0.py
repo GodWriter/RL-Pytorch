38ad938bf858652d6ba11ed9b727ee705ca5108a
GodWriter <18917565247@163.com>
2020-11-23 15:52:03 +0800

    commit.

--- value_function_approximate/Prioritized_DQN/Prioritized_DQN_CartPole_V0.py
+++ value_function_approximate/Prioritized_DQN/Prioritized_DQN_CartPole_V0.py
@@ -1,4 +1,5 @@
 import gym
+import math
 import argparse
 import numpy as np
 
@@ -22,6 +23,23 @@ device = 'cpu' if not torch.cuda.is_available() else 'cuda:0'
 device = torch.device(device)
 
 
+class NaivePrioritizedBuffer(object):
+    def __init__(self, capacity, prob_alpha=0.6):
+        pass
+
+    def push(self, state, action, reward, next_state, done):
+        pass
+
+    def sample(self, batch_size, beta=0.4):
+        pass
+
+    def update_priorities(self, batch_indices, batch_priorities):
+        pass
+
+    def __len__(self):
+        return len(self.buffer)
+
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -36,73 +54,84 @@ class Net(nn.Module):
         return self.layers(x)
 
 
-class PDQN():
+class PrioritizedDQN():
     def __init__(self, args):
-        super(DQN, self).__init__()
+        super(PrioritizedDQN, self).__init__()
         self.args = args
 
-        self.target_net = Net()
-        self.eval_net = Net()
-        self.memory = [None] * self.args.capacity
-
-        self.loss_func = nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), self.args.learning_rate)
+        self.target_net = Net().to(device)
+        self.eval_net = Net().to(device)
 
         self.memory_count = 0
         self.update_count = 0
+        self.replay_buffer = NaivePrioritizedBuffer(self.args.capacity)
+
         self.writer = SummaryWriter(self.args.logs)
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), self.args.learning_rate)
 
-    def store_transition(self, transition):
-        idx = self.memory_count % self.args.capacity
+    def choose_action(self, state, epsilon):
+        if np.random.rand(1) > epsilon:
+            state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
+            value = self.eval_net(state)
 
-        self.memory[idx] = transition
-        self.memory_count += 1
-
-        return self.memory_count >= self.args.capacity
-
-    def choose_action(self, state):
-        state = torch.tensor(state, dtype=torch.float).unsqueeze(0)
-        value = self.eval_net(state)
-
-        _, idx = torch.max(value, 1)
-        action = idx.item() # Important, get the action idx outside the graph
-
-        if np.random.rand(1) >= 0.9: # epsilon greedy
+            _, idx = torch.max(value, 1)
+            action = idx.item()
+        else:
             action = np.random.choice(range(num_action), 1).item()
 
         return action
 
-    def learn(self):
+    def learn(self, beta):
         if self.memory_count >= self.args.capacity:
-            state = torch.tensor([t.state for t in self.memory]).float()
-            action = torch.LongTensor([t.action for t in self.memory]).view(-1, 1).long()
-            reward = torch.tensor([t.reward for t in self.memory]).float()
-            next_state = torch.tensor([t.next_state for t in self.memory]).float()
+            state, action, reward, next_state, done, indices, weights = self.replay_buffer.sample(self.args.batch_size, beta)
 
-            with torch.no_grad(): # 对target_v计算中所有涉及到的图节点不进行更新
-                target_v = reward + self.args.gamma * self.target_net(next_state).max(1)[0]
+            state = torch.FloatTensor(np.float32(state)).to(device)
+            next_state = torch.FloatTensor(np.float32(next_state)).to(device)
+            action = torch.LongTensor(action).to(device)
+            reward = torch.FloatTensor(reward).to(device)
+            done = torch.FloatTensor(done).to(device)
+            weights = torch.FloatTensor(weights).to(device)
 
-            # Training
-            for idx in BatchSampler(SubsetRandomSampler(range(len(self.memory))),
-                                    batch_size=self.args.batch_size,
-                                    drop_last=False):
-                eval_v = (self.eval_net(state).gather(1, action))[idx]
-                loss = self.loss_func(target_v[idx].unsqueeze(1), eval_v)
+            eval_v = self.eval_net(state)
+            target_v = self.target_net(next_state)
 
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+            q_v = eval_v.gather(1, action.unsqueeze(1)).squeeze(1)
+            next_q_v = target_v.max(1)[0]
+            expected_q_v = reward + self.args.gamma * next_q_v * (1 - done)
 
-                self.writer.add_scalar('loss/value_loss', loss, self.update_count)
-                self.update_count += 1
+            loss = (q_v - expected_q_v.detach()).pow(2) * weights
+            prios = loss + 1e-5
+            loss = loss.mean()
 
-                if self.update_count % 100 == 0:
-                    self.target_net.load_state_dict(self.eval_net.state_dict())
-        else:
-            print("Memory Buff size is {}, Now is collecting more...".format(self.memory_count))
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            self.replay_buffer.update_priorities(indices, prios.detach().cpu().numpy())
+            self.optimizer.step()
+
+            self.writer.add_scalar('loss/value_loss', loss, self.update_count)
+            self.update_count += 1
+
+            if self.update_count % 100 == 0:
+                self.target_net.load_state_dict(self.eval_net.state_dict())
 
 
-def reward_func(env, x, x_dot, theta, theta_dot):
+def epsilon_by_frame(frame_idx):
+    epsilon_start = 1.0
+    epsilon_final = 0.01
+    epsilon_decay = 500
+
+    return epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+
+def beta_by_frame(frame_idx):
+    beta_start = 0.4
+    beta_frames = 1000
+
+    return min(1.0, beta_start + frame_idx * (1.0 - beta_start) / beta_frames)
+
+
+def reward_func(env, x, theta):
     r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.5
     r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
 
@@ -110,47 +139,52 @@ def reward_func(env, x, x_dot, theta, theta_dot):
 
 
 def main(args):
-    agent = DQN(args)
+    agent = PrioritizedDQN(args)
 
-    for n_ep in range(args.n_episodes):
-        state = env.reset()
-        ep_reward = 0.0 # 记录一个episode能够拿到的整体收益
+    ep_reward = 0.0
+    state = env.reset()
+    if args.render: env.render()
+
+    for f_idx in range(args.n_frames):
+        # choose action and get next_state
+        epsilon = epsilon_by_frame(f_idx)
+        action = agent.choose_action(state, epsilon)
+        next_state, _, done, info = env.step(action)
+
+        # compute the reward with the state
+        x, _, theta, _ = next_state
+        reward = reward_func(env, x, theta)
+
+        # update the replay buffer.
         if args.render: env.render()
+        agent.replay_buffer.push(state, action, reward, next_state, done)
 
-        for t in range(10000):
-            action = agent.choose_action(state)
-            next_state, _, done, info = env.step(action)
+        state = next_state
+        ep_reward += reward
 
-            x, x_dot, theta, theta_dot = next_state
-            reward = reward_func(env, x, x_dot, theta, theta_dot)
-            ep_reward += reward
+        if len(agent.replay_buffer) > args.batch_size:
+            beta = beta_by_frame(f_idx)
+            agent.learn(beta)
 
-            if args.render: env.render()
-            transition = Transition(state, action, reward, next_state)
-            agent.store_transition(transition)
+        if f_idx % 200 == 0:
+            print("Frame is {}, the episode reward is {}".format(f_idx, round(ep_reward, 3)))
 
-            if done or t >= 9999:
-                agent.writer.add_scalar('live/finish_step', t+1, global_step=n_ep) # 记录每一轮在第几步结束
-                agent.learn()
-
-                if n_ep % 10 == 0:
-                    print("episode {}, step is {}, the episode reward is {}".format(n_ep, t, round(ep_reward, 3)))
-                break
-
-            state = next_state
+        if done:
+            state = env.reset()
+            ep_reward = 0.0
 
     print("Training is Done!!!")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--n_episodes', type=int, default=2000)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--n_frames', type=int, default=1000000)
     parser.add_argument('--capacity', type=int, default=8000)
-    parser.add_argument('--gamma', type=float, default=0.995)
+    parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--render', type=bool, default=True)
-    parser.add_argument('--logs', type=str, default='logs/DQN_CartPole_V0')
+    parser.add_argument('--logs', type=str, default='logs/Prioritized_DQN_CartPole_V0')
     args = parser.parse_args()
     print(args)
 
